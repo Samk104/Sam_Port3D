da7beee6519d16d4fa9fe12e64a854101f3278c7
Samujjwal Kumar <kumarsamujjwal@gmail.com>
2024-10-02 15:54:49 -0400

    REDS-2024 Updated intro line-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white `}>Hi, I'm <span className="text-[#915eff]">Sam</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I love solving problems<br className="sm:block hidden" />and developing web and mobile applications
+            I love solving problems<br className="sm:block hidden" />and developing web, mobile applications
           </p>
         </div>
         </div>
